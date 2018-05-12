1921e8401e686da45c27c4ec4590984e66123797
Sergiy Matusevych <sergiy.matusevych@gmail.com>
2018-05-11 17:08:55 -0700

    working version of new gen_data script

--- python/gen_data.py
+++ python/gen_data.py
@@ -33,78 +33,88 @@ class GenerationOpts:
         self.p_geo = opts.p_geo
 
 
-def mk_dataset_filename(opts, n_batches):
-    return "%s/npb=%d_nb=%d.pkl" % (opts.out_dir, opts.max_nodes_per_batch, n_batches)
+def get_splits(n_pairs, min_n, max_n):
+    return list(zip(range(min_n, max_n),
+                    sorted(random.randint(0, n_pairs)
+                           for _ in range(max_n - min_n)))) \
+           + [(max_n, n_pairs)]
+
+
+def mk_dataset_filename(opts, n_batches, n_file):
+    return "%s/f=%04dnpb=%04d_nb=%04d.pkl" % (opts.out_dir, n_file, opts.max_nodes_per_batch, n_batches)
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--max_nodes_per_batch', action='store', type=int)
+parser.add_argument('--max_batches_per_file', action='store', type=int, default=1000)
 parser.add_argument('--one', action='store', dest='one', type=int, default=0)
 
 opts = init_opts(parser)
 
-n_batch = 0
-n_nodes_in_batch = 0
-gen_opts = GenerationOpts(opts)
-
-
-if pair % opts.print_interval == 0:
-    print("[%d]" % pair)
-
-batch_range = 0.3
-n_pairs_per_var = opts.n_pairs / (opts.max_n - opts.min_n + 1)
-n_pairs_rem = opts.n_pairs
-
-for n_vars in range(opts.min_n, opts.max_n + 1):
-
-    n_pairs = min(
-        n_pairs_rem,
-        random.randint(n_pairs_per_var * (1.0 - batch_range),
-                       n_pairs_per_var * (1.0 + batch_range)))
-
-    for pair in range(n_pairs):
-
-        gen_opts.min_n = n_vars
-        gen_opts.max_n = n_vars
-        _, iclauses_unsat, iclauses_sat = gen_iclause_pair2(gen_opts)
-
-    for iclauses in (iclauses_unsat, iclauses_sat):
-        n_clauses = len(iclauses)
-        n_cells = sum([len(iclause) for iclause in iclauses])
-
-        n_nodes = 2 * n_vars + n_clauses
-        if n_nodes > opts.max_nodes_per_batch:
-            continue
-
-        batch_ready = False
-        if (opts.one and len(problems) > 0):
-            batch_ready = True
-        elif (prev_n_vars and n_vars != prev_n_vars):
-            batch_ready = True
-        elif (not opts.one) and n_nodes_in_batch + n_nodes > opts.max_nodes_per_batch:
-            batch_ready = True
-
-        if batch_ready:
-            batches.append(mk_batch_problem(problems))
-            print("batch %d done (%d vars, %d problems)...\n" % (len(batches), n_vars, len(problems)))
-            del problems[:]
-            n_nodes_in_batch = 0
-
-        prev_n_vars = n_vars
-
-        is_sat, stats = solve_sat(n_vars, iclauses)
-        problems.append((filename, n_vars, iclauses, is_sat))
-        n_nodes_in_batch += n_nodes
-
-if len(problems) > 0:
-    batches.append(mk_batch_problem(problems))
-    print("batch %d done (%d vars, %d problems)...\n" % (len(batches), n_vars, len(problems)))
-    del problems[:]
 
 # create directory
 if not os.path.exists(opts.out_dir):
     os.mkdir(opts.out_dir)
 
-dataset_filename = mk_dataset_filename(opts, len(batches))
-print("Writing %d batches to %s...\n" % (len(batches), dataset_filename))
-with open(dataset_filename, 'wb') as f_dump:
-    pickle.dump(batches, f_dump)
+
+batches = []
+problems = []
+n_batch = 1
+n_file = 1
+n_nodes_in_batch = 0
+
+gen_opts = GenerationOpts(opts)
+prev_pairs = 0
+
+for n_vars, total_pairs in get_splits(opts.n_pairs, opts.min_n, opts.max_n):
+
+    for pair in range(total_pairs - prev_pairs + 1):
+
+        gen_opts.min_n = n_vars
+        gen_opts.max_n = n_vars
+        _, iclauses_unsat, iclauses_sat = gen_iclause_pair2(gen_opts)
+
+        n_clauses = len(iclauses_sat)  # same number for sat/unsat
+        n_cells = sum([len(iclause) for iclause in iclauses_sat])
+        n_nodes = 2 * n_vars + n_clauses
+
+        if n_nodes > opts.max_nodes_per_batch:
+            continue
+
+        for iclauses in (iclauses_unsat, iclauses_sat):
+
+            is_sat, stats = solve_sat(n_vars, iclauses)
+            problems.append((None, n_vars, iclauses, is_sat))
+            n_nodes_in_batch += n_nodes
+
+            if opts.one or n_nodes_in_batch >= opts.max_nodes_per_batch:
+                batches.append(mk_batch_problem(problems))
+                print("batch %4d done (%2d vars, %6d problems)..." % (n_batch, n_vars, len(problems)))
+                problems = []
+                n_nodes_in_batch = 0
+                n_batch += 1
+
+                if len(batches) >= opts.max_batches_per_file:
+                    dataset_filename = mk_dataset_filename(opts, len(batches), n_file)
+                    print("# Writing %d batches to %s..." % (len(batches), dataset_filename))
+                    with open(dataset_filename, 'wb') as f_dump:
+                        pickle.dump(batches, f_dump)
+                    batches = []
+                    n_file += 1
+
+    prev_pairs = total_pairs
+
+    if problems:
+        batches.append(mk_batch_problem(problems))
+        print("batch %4d done (%2d vars, %6d problems)..." % (n_batch, n_vars, len(problems)))
+        problems = []
+        n_nodes_in_batch = 0
+        n_batch += 1
+
+if len(batches):
+    dataset_filename = mk_dataset_filename(opts, len(batches), n_file)
+    print("# Writing %d batches to %s..." % (len(batches), dataset_filename))
+    with open(dataset_filename, 'wb') as f_dump:
+        pickle.dump(batches, f_dump)
+    batches = []
+    n_file += 1
