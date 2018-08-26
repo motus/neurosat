7b746f5e6e22f220955a2f3176fe6dc1167ebf64
Sergiy Matusevych <sergiy.matusevych@gmail.com>
2018-08-25 18:53:30 -0700

    minor fixes in training output

--- python/neurosat.py
+++ python/neurosat.py
@@ -199,14 +199,13 @@ class NeuroSAT(object):
 
         train_problems, train_filename = self.train_problems_loader.get_next()
         for (i, problem) in enumerate(train_problems, 1):
-            print("Training on batch %5d of %d..." % (i, len(train_problems)))
-            batch_start = time.clock()
+            print("Batch %5d of %d..." % (i, len(train_problems)), end='\r')
             d = self.build_feed_dict(problem)
             _, logits, cost = self.sess.run([self.apply_gradients, self.logits, self.cost], feed_dict=d)
             epoch_train_cost += cost
             epoch_train_mat.update(problem.is_sat, logits > 0)
-            print("Training on batch %5d done in %.2fs" % (i, time.clock() - batch_start))
 
+        print("")
         epoch_train_cost /= len(train_problems)
         epoch_train_mat = epoch_train_mat.get_percentages()
         epoch_end = time.clock()
