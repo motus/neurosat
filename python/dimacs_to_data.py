b14d24965b914271fcc630bba4f567be09525a82
Sergiy Matusevych <sergiy.matusevych@gmail.com>
2018-09-17 19:13:09 -0700

    print the failed line and file

--- python/dimacs_to_data.py
+++ python/dimacs_to_data.py
@@ -25,16 +25,20 @@ from solver import solve_sat
 from mk_problem import mk_batch_problem
 
 def parse_dimacs(filename):
-    with open(filename, 'r') as f:
-        lines = f.readlines()
-    i = 0
-    while lines[i].strip().split(" ")[0] == "c":
-        i += 1
-    header = lines[i].strip().split(" ")
-    assert(header[0] == "p")
-    n_vars = int(header[2])
-    iclauses = [[int(s) for s in line.strip().split(" ")[:-1]] for line in lines[i+1:]]
-    return n_vars, iclauses
+    try:
+        with open(filename, 'r') as f:
+            lines = f.readlines()
+        i = 0
+        while lines[i].strip().split(" ")[0] == "c":
+            i += 1
+        header = lines[i].strip().split(" ")
+        assert(header[0] == "p")
+        n_vars = int(header[2])
+        iclauses = [[int(s) for s in line.strip().split(" ")[:-1]] for line in lines[i+1:]]
+        return n_vars, iclauses
+    except Exception as ex:
+        sys.stderr.write("Error parsing %s :: %s\n" % (filename, ex))
+        raise
 
 def mk_dataset_filename(opts, n_batches):
     dimacs_path = opts.dimacs_dir.split("/")
