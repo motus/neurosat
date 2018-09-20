70ac4af3aaefe6b4e63781b6cab31ac436076b43
Sergiy Matusevych <sergiy.matusevych@gmail.com>
2018-09-20 15:41:58 -0700

    split neurostat stats collection into 2 versions + minor fixes

--- scripts/neurosat-solve-stats-2.py
+++ scripts/neurosat-solve-stats-2.py
@@ -2,19 +2,16 @@
 
 import re
 import sys
-from collections import defaultdict
 
 
-_RE_FNAME = re.compile(r'.*neurosat-experiment-1-(\d+)-(\d+)\.log$')
 _RE_SAT = re.compile(r'_sat=([01])\.dimacs\]')
 _RE_VALID = re.compile(r'\((True|False), ([N[])')
 
-sat_solved = defaultdict(set)
-sat_all = defaultdict(set)
+sat_solved = set()
+sat_all = set()
+sat_invalid = set()
 for fname in sys.argv[1:]:
     sys.stderr.write("%-64s\r" % fname)
-    [(n_vars, _n_iter)] = _RE_FNAME.findall(fname)
-    n_vars = int(n_vars)
     with open(fname) as fh_input:
         for line in fh_input:
             split = line.split(' ', 1)
@@ -26,17 +23,18 @@ for fname in sys.argv[1:]:
                 continue
             is_sat = int(is_sat[0])
             if is_sat:
-                sat_all[n_vars].add(key)
+                sat_all.add(key)
             [(is_valid, has_solution)] = _RE_VALID.findall(val)
             is_valid = is_valid == 'True'
             has_solution = has_solution == '['
             if is_sat and is_valid and has_solution:
-                sat_solved[n_vars].add(key)
+                sat_solved.add(key)
+            if not is_valid:
+                sat_invalid.add(key)
 
 sys.stderr.write("\n")
 
-print("n_vars,sat,sat_good,sat_good_pct")
-for n_vars in [20, 40, 60, 80]:
-    n_sat = len(sat_all[n_vars])
-    n_solved = len(sat_solved[n_vars])
-    print("%d,%d,%d,%.2f" % (n_vars, n_sat, n_solved, n_solved * 100.0 / n_sat))
+print("sat,sat_invalid,sat_good,sat_good_pct")
+n_sat = len(sat_all)
+n_solved = len(sat_solved)
+print("%d,%d,%d,%.2f" % (n_sat, len(sat_invalid), n_solved, n_solved * 100.0 / n_sat))
